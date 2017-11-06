9232dadec5b863ae576a374f8440ea1c5f5d39ff
furu8 <foooolurk1125@gmail.com>
2017-11-07 03:59:08 +0900

    家計簿の追加内容をindexに表示させる

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -1,5 +1,22 @@
 class WelcomesController < ApplicationController
+
   def index
-    @welcomes = Welcome.all.order(created_at: 'desc')
+    @welcomes = Welcome.all
   end
+
+  def new
+  
+  end
+
+  def create
+    @welcome = Welcome.new(welcome_params)
+    @welcome.save
+    redirect_to welcomes_path
+  end
+
+  private
+  def welcome_params
+    params.require(:welcome).permit(:week, :money)
+  end
+
 end
